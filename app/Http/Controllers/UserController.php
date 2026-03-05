bca29d615f1b2c48584bbb01771d2a506171acfd
Sharren <sharrenepf@gmail.com>
2026-03-05 21:13:04 +0700

    commit 5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    //
+    public function index()
+    {
+        // tambah data user dengan Eloquent Model
+        $data = [
+            'nama' => 'Pelanggan Pertama',
+        ];
+        
+        // Update data user berdasarkan username 'customer-1'
+        UserModel::where('username', 'customer-1')->update($data); 
+
+        // coba akses model UserModel
+        $user = UserModel::all(); // ambil semua data dari tabel m_user
+        return view('user', ['data' => $user]);
+    }
 }
