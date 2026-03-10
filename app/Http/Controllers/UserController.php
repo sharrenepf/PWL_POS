755da7c05cfb9577754fb8a238669ac7f90fe4c4
Sharren <sharrenepf@gmail.com>
2026-03-11 00:04:24 +0700

    js 3 percobaan 2.5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,16 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserModel; // Supaya UserModel terbaca
+use Illuminate\Support\Facades\Hash; // Supaya fungsi Hash::make terbaca
 use Illuminate\Http\Request;
-use App\Models\UserModel;
-use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::where('level_id', 2)->count();
-        //dd($user);
-        return view('user', ['data' => $user]);
+        $user = UserModel::create([
+            'username' => 'manager11',
+            'nama' => 'Manager11',
+            'password' => Hash::make('12345'),
+            'level_id' => 2,
+        ]);
+
+        $user->username = 'manager12';
+
+        $user->save();
+
+        $user->wasChanged(); // true
+        $user->wasChanged('username'); // true
+        $user->wasChanged(['username', 'level_id']); // true
+        $user->wasChanged('nama'); // false
+        dd($user->wasChanged(['nama', 'username'])); // true
     }
 }
